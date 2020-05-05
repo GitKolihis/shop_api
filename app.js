89bd8dfe24867b74d149d12d901d254da01c7de2
GitKolihis <46547971+GitKolihis@users.noreply.github.com>
2020-05-05 05:22:16 +0100

    adding jwt to protect api routes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.Promise = global.Promise;
 app.use(morgan("dev"));
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
